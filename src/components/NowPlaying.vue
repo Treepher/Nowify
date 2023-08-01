3a0f53898a13bb807f0636bbf25da07277a8eed8
Taran Ramu <61845587+Treepher@users.noreply.github.com>
2023-08-01 17:08:31 -0400

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -179,7 +179,7 @@ export default {
       clearInterval(this.pollPlaying)
       this.pollPlaying = setInterval(() => {
         this.getNowPlaying()
-      }, 2500)
+      }, 2000)
     },
 
     /**
